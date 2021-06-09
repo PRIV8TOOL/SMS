e73b97285d0217238f4a4dbab31353c0c9a9f0e0
W3LL STORE <81862344+W3LLLL@users.noreply.github.com>
2021-06-10 05:57:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,16 @@ printf "         \033[0;36m\n\n"
 sudo apt install -y screen
 sudo apt install -y unzip
 sudo apt-get update
-sudo apt-get install -y software-properties-common
 sudo locale-gen en_US.UTF-8
 sudo export LANG=en_US.UTF-8
-sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y figlet
 sudo apt-get install -y dos2unix
 sudo apt-get install -y curl
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
 sudo apt-get install -y php5.6
 sudo apt-get install -y php5.6-curl
 sudo apt install -y nano
